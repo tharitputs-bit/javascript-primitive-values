2106665c26358972478b4b36a41de61a24a06d8e
AnthonyFS <anthonyfungesmith@gmail.com>
2026-06-02 13:28:50 +0700

    completed assignment

--- ex-4.js
+++ ex-4.js
@@ -5,3 +5,5 @@ let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
 
 console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+
+//Nan
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = 1 - 0.25;
+let actualPrice = totalPrice * discount;
+console.log(actualPrice);
