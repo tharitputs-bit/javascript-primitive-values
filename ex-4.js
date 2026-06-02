2c7e44540f0831d17568871131ed8e17f6ef18f3
tharit <tharit.puts@gmail.com>
2026-06-02 13:50:13 +0700

    feat: add new variables and calculations in exercises

--- ex-4.js
+++ ex-4.js
@@ -4,4 +4,5 @@
 let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
 
-console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+console.log((numberOfSoldProduct / numberOfProductInStock) * 100); 
+// the result is NaN (Not a Number)
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = totalPrice * 0.25;
+let finalPrice = discount;
+console.log(finalPrice);
